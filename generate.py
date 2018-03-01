64ce5bfe275d3611296023d50957ede2cca45bf4
Josh <jxm7374@rit.edu>
2018-02-28 22:50:16 -0500

    A ton of changes

--- generate.py
+++ generate.py
@@ -1,49 +1,23 @@
-from keras.models import Sequential, load_model
-from keras.layers import LSTM, Dropout, TimeDistributed, Dense, Activation, Embedding
-import numpy as np
+from generator import generate
 import argparse
-def build_sample_model(vocab_size,emb_size=100,num_layers=1,hidden_size=100,dropout=0.2):
-	model = Sequential()
-	model.add(Embedding(vocab_size,emb_size,batch_input_shape=(1,1)))
-	for i in range(num_layers):
-		model.add(LSTM(hidden_size,return_sequences=(i<num_layers-1),stateful=True))
-		model.add(Dropout(dropout))
+import random
+import string
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--seed",default="random")
+	parser.add_argument("--maxlen",default=50,type=int)
+	parser.add_argument("--numnames",default=1,type=int)
+	args = parser.parse_args()
+	seed = args.seed#"Q"
+	maxlen = args.maxlen#50
+	number_of_names = args.numnames#1
+	for i in range(number_of_names):
+		if args.seed == "random":
+			seed = random.choice(list(string.ascii_uppercase))
+		name = generate.sample(seed,maxlen)
+		print(name)
 
-	model.add(Dense(vocab_size))
-	model.add(Activation('softmax'))
-	return model
 
-def sample(epoch, header, num_chars):
-	text = open("names.txt").read()
-	char_to_ix = {ch:i for (i,ch) in enumerate(sorted(list(set(text))))}
-	ix_to_char = {i:ch for (ch,i) in char_to_ix.items()}
-	vocab_size = len(char_to_ix)
-	model = build_sample_model(vocab_size)
-	#load_weights(epoch, model)
-	model.load_weights('weights.h5')
-
-	sampled = [char_to_ix[c] for c in header]
-	for c in header[:-1]:
-		batch = np.zeros((1, 1))
-		batch[0, 0] = char_to_ix[c]
-		model.predict_on_batch(batch)
-
-	for i in range(num_chars):
-		batch = np.zeros((1, 1))
-		if sampled:
-			batch[0, 0] = sampled[-1]
-		else:
-			batch[0, 0] = np.random.randint(vocab_size)
-		result = model.predict_on_batch(batch).ravel()
-		sample = np.random.choice(range(vocab_size), p=result)
-		if ix_to_char[sample] == "\n":
-			break
-		sampled.append(sample)
-
-	return ''.join(ix_to_char[c] for c in sampled)
-
-text = open("names.txt").read()
-for i in range(10):
-	name = sample(10000,"Z",50)
-	print(name +" "+ str(name in text))
+if __name__ == "__main__":
+	main()
 
--- readme.md
+++ readme.md
@@ -9,4 +9,12 @@ Don't use keras as much as tensorflow, so writing this was made a lot easier whe
 Coming soon... :pizza:
 
 ## How to use
-Working on it... :alien:
+In order to run, simply call `python generate.py` <br />
+The generator comes with a few extra arguments in order to specify certain constraints you put on generation.<br />
+With this generator you can specify three things:
+- `--seed` is the starting character(s) you'd like the generator to use when making a name. If you leave this empty, the generator will choose a random capital letter to start every name it generates.
+- `--numnames` is the number of names you'd like to generate, if you want to generate a batch.
+- `--maxlen` this is the constraint on maximum length of a name. I find it's better not to touch this, but included it as an option anyways.
+<br />
+Here's one more example for using all of the above arguments: `python generate.py --seed J --numnames 10 --maxlen 10`<br />
+This sample will make 10 names starting with the letter J and will cut them off at a max of 10 characters long
